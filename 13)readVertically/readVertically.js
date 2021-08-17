f08b63157c1c115df6ec7108e8f6a0278a77885f
Richard <81045794+hyukzz@users.noreply.github.com>
2021-08-17 20:50:21 +0900

    알고리즘 문제 13번

---
+++ 13)readVertically/readVertically.js
@@ -0,0 +1,20 @@
+function readVertically(arr) {
+    let temp = [];
+    for (let i = 0; i < arr.length; i++) {
+      let str = arr[i];
+      for (let j = 0; j < str.length; j++) {
+        if (temp.length === j) {
+          temp.push(str[j]);
+        } else {
+          temp[j] = temp[j] + str[j];
+        }
+      }
+    }
+  
+    let result = '';
+    for (let i = 0; i < temp.length; i++) {
+      result = result + temp[i];
+    }
+  
+    return result;
+  }
