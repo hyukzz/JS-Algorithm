fff39fb55d1e2a65b7a92a0ec7158ee14ea4ab15
Richard <81045794+hyukzz@users.noreply.github.com>
2021-08-18 18:33:39 +0900

    알고리즘 문제 14번

---
+++ 14)superIncreasing/superIncreasing.js
@@ -0,0 +1,11 @@
+function superIncreasing(arr) {
+    let sum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] <= sum) {
+        return false;
+      }
+      sum = sum + arr[i];
+    }
+    return true;
+  }
+  
