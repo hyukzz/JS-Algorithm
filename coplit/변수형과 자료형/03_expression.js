124742f391cbea1be9134b1a154c3ae24f249d71
Richard <81045794+hyukzz@users.noreply.github.com>
2021-08-24 21:25:54 +0900

    변수와 자료형 1~8

---
+++ coplit/변수형과 자료형/03_expression.js
@@ -0,0 +1,9 @@
+// 값의 표현(expression)
+// 표현식(expression)은 하나의 값으로 표현되는 코드를 의미합니다.
+
+// TODO : 변수 num1에 숫자 5를 할당하고, 변수 num2에 숫자 7을 할당한 후, 변수 result에 숫자가 할당된 변수 num1과 num2의 곱을 할당합니다.
+let num1 = 5;
+let num2 = 7;
+let result = num1 * num2
+
+
