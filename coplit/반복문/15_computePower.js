5c6e896a0067c579be35502070729132b391d9ee
hyukzz <ri_chard@naver.com>
2021-09-01 18:07:33 +0900

    Coplit_반복문_15

---
+++ coplit/반복문/15_computePower.js
@@ -0,0 +1,23 @@
+// 문제
+// 밑(base)과 지수(exponent)를 입력받아 밑의 거듭제곱을 리턴해야 합니다.
+
+// 입력
+// 인자 1 : base
+// number 타입의 밑
+// 인자 2 : exponent
+// number 타입의 지수
+// 출력
+// number 타입을 리턴해야 합니다.
+// 주의 사항
+// 반복문(for)문을 사용해야 합니다.
+// Math.pow, 거듭제곱 연산자 사용은 금지됩니다. (거듭제곱은 자바스크립트 표준 내장 객체인 Math 객체를 통해 쉽게 구할 수 있습니다.)
+
+function computePower(base, exponent) {
+    let result = 1;
+  
+    for (let i = 0; i < exponent; i++) {
+      result = result * base;
+    }
+  
+    return result;
+  }
