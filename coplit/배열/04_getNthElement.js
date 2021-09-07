93a39e7ced65ccd41d5b562b60d387aafeaedddb
hyukzz <ri_chard@naver.com>
2021-09-07 21:00:35 +0900

    Coplit_배열_4

---
+++ coplit/배열/04_getNthElement.js
@@ -0,0 +1,33 @@
+// 문제
+// 배열과 수를 입력받아 수가 인덱스로 가리키는 배열의 요소를 리턴해야 합니다.
+
+// 입력
+// 인자 1 : arr
+// 임의의 요소를 갖는 배열
+// 인자 2 : index
+// number 타입의 index (0 이상의 정수)
+// 출력
+// 배열의 요소를 리턴해야 합니다.
+// 주의 사항
+// 빈 배열을 입력받은 경우, undefined를 리턴해야 합니다.
+// 배열의 길이를 벗어나는 수를 입력받은 경우, 'out of index range'를 리턴해야 합니다.
+// 입출력 예시
+// let output = getNthElement([1, 3, 5], 1);
+// console.log(output); // --> 3
+
+// output = getNthElement([1, 3, 5], 3);
+// console.log(output); // --> 'out of index range'
+
+function getNthElement(arr, index) {
+    // arr=[]일때 undefined
+    //arr.length< index 면 '~'
+    if(arr.length === 0){
+      return undefined;
+    } else if(arr.length <= index) {
+      return 'out of index range';
+    } else {
+      return arr[index];
+    }
+  }
+  
+  
